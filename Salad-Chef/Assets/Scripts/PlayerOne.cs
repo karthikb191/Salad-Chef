75134f2d3674980533ad548c3fe0006ca0721b61
DESKTOP-14HFB0R\Karthik B <karthik.busam@thinktac.com>
2019-03-17 12:34:20 +0530

    Implemented movement and generic pickup functionality

---
+++ Salad-Chef/Assets/Scripts/PlayerOne.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode action;
+    public KeyCode pickUpSalad;
+
+    public float speed = 10;    //Default speed of the player
+    public int maxCarryCapacity = 2;
+
+    float multiplier = 1;
+
+    Vector3 Move()
+    {
+        Vector3 displacement = Vector3.zero;
+        bool keyPressed = false;
+        
+
+        if (Input.GetKey(up)) { displacement.y += multiplier * speed * Time.deltaTime; keyPressed = true; }
+        if (Input.GetKey(down)) { displacement.y -= multiplier * speed * Time.deltaTime; keyPressed = true; }
+        if (Input.GetKey(left)) { displacement.x -= multiplier * speed * Time.deltaTime; keyPressed = true; }
+        if (Input.GetKey(right)) { displacement.x += multiplier * speed * Time.deltaTime; keyPressed = true; }
+
+        //TODO: ADD the smooth movement logic and direction interpolation
+        //TODO: Fix unity collider issue
+        if (keyPressed)
+        {
+
+        }
+        else
+        {
+
+        }
+
+        return displacement;
+    }
+
+    List<Object> itemsCarrying = new List<Object>();
+
+    public delegate void Action();
+    public event Action ActionEvent;
+
+    IInteractable interactableObjectOnFocus;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        IInteractable i = collider.GetComponent<IInteractable>();
+        if(i != null)
+        {
+            Debug.Log("Came into contact with pickable object");
+            interactableObjectOnFocus = i;
+        }
+        else
+        {
+            Debug.Log("meh");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        IInteractable i = collider.GetComponent<IInteractable>();
+        if (interactableObjectOnFocus == i)
+        {
+            interactableObjectOnFocus = null;
+        }
+    }
+
+    protected void Start()
+    {
+        //itemsCarrying = new ArrayList(2);
+    }
+
+    protected void Update()
+    {
+        gameObject.transform.Translate(Move());
+
+        if (Input.GetKeyDown(action))
+        {
+            Interact(interactableObjectOnFocus);
+            //PickUp(pickableObjectOnFocus);
+        }
+    }
+
+    public List<Object> GetItemsCarrying()
+    {
+        return itemsCarrying;
+    }
+
+    public void Interact(IInteractable obj)
+    {
+        if(obj != null)
+        {
+            Debug.Log("Executing the interaction");
+            obj.Interact(this);
+        }
+        else
+        {
+            Debug.Log("Interactable object is null");
+        }
+    }
+
+    public void AddToItems(Object obj)
+    {
+        if(itemsCarrying.Count >= maxCarryCapacity)
+        {
+            Debug.Log("Carry capacity at maximum");
+            return;
+        }
+
+        //Check to see if the vegetable we are trying to pick up is already with the player
+        Vegetable v = obj as Vegetable;
+        if (v)
+        {
+            Debug.Log("vegeable is: " + v.veggieType);
+            Debug.Log("Trying to pickup a vegetable");
+            for(int i = 0; i < itemsCarrying.Count; i++)
+            {
+                Vegetable v2 = itemsCarrying[i] as Vegetable;
+                if (v2)
+                {
+                    if(v.veggieType == v2.veggieType)
+                    {
+                        Debug.Log("Vegetable already being carried by player. No need to add again");
+                        return;
+                    }
+                }
+            }
+            //If the vegetable type is not being carried, then add it to the player's list
+            itemsCarrying.Add(obj);
+            return;
+        }
+        
+        Salad s = obj as Salad;
+        if (s)
+        {
+            //No special conditions required for salad as it's only possible to make one salad at a time
+            Debug.Log("Trying to pickup the salad");
+            itemsCarrying.Add(obj);
+        }
+        
+    }
+
+    public void PowerUp()
+    {
+
+    }
+
+    
+    public void RemoveSalad()
+    {
+        for(int i = 0; i < itemsCarrying.Count; i++)
+        {
+
+        }
+        Debug.Log("No salad to remove");
+    }
+
+    public void RemoveVegetable()
+    {
+
+    }
+
+}
+
+public class PlayerOne : Player {
+
+    //Constructor defines the default controls of the player
+    public PlayerOne()
+    {
+        up = KeyCode.W;
+        down = KeyCode.S;
+        left = KeyCode.A;
+        right = KeyCode.D;
+        action = KeyCode.X;
+        pickUpSalad = KeyCode.C;
+    }
+
+	// Use this for initialization
+	void Start () {
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        base.Update();
+	}
+}
---
+++ Salad-Chef/Assets/Scripts/Vegetable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Veggie
+{
+    Tomato,
+    Potato,
+    Cucumber,
+    Banana,
+    Raddish,
+    Carrot
+}
+
+public interface IInteractable
+{
+    void Interact(Player p);
+}
+
+public interface IPickable : IInteractable
+{
+    void Pick(Player p);
+}
+
+public class Vegetable : MonoBehaviour, IPickable {
+    bool shredded = false;
+
+    public Veggie veggieType = Veggie.Tomato;
+    
+    public void Interact(Player p)
+    {
+        Pick(p);
+    }
+
+    public void Pick(Player p)
+    {
+        //Instantiate a copy and send to the player
+        p.AddToItems(this);
+    }
+
+}
